959f5952ffc74896bacb4d36eabf39fcccd25072
Shaun Naude <shaunnaude20@gmail.com>
2020-04-17 13:44:30 +0200

    done fitness

--- Prac2/src/geneticAlgorithm.cpp
+++ Prac2/src/geneticAlgorithm.cpp
@@ -61,12 +61,13 @@ inline void geneticAlgorithm::addData(string line)
 
 void geneticAlgorithm::createPopulation(){
 
+    shared_ptr<puzzle> temp;
    for(int i = 0 ; i<16 ; i++)
    {
-       shared_ptr temp = make_shared<puzzle>();
+     temp = make_shared<puzzle>();
        for(int row = 0; row<9 ; row++)
        {
-           for(int col = 0; col<9 ;row++)
+           for(int col = 0; col<9 ;col++)
            {
                if(Input->numbers[row][col] != 0)
                {
@@ -80,6 +81,7 @@ void geneticAlgorithm::createPopulation(){
        }
 
         temp->fitness = calcFitness(temp);
+        Population.push(temp);
    }
    
 
@@ -91,8 +93,114 @@ void geneticAlgorithm::createPopulation(){
 
 inline int geneticAlgorithm::calcFitness(shared_ptr<puzzle> Curr)
 {
+
+//////////////////////////////////////////////////////
+    int temp[9];
+    unordered_set <int> intSet ;
+    unordered_set<int> duplicate;
+    int fitness = 0;
     //this calculates the individual puzzles fitness
-}
+
+    //lets do rows first
+
+    for(int row = 0 ; row<9 ; row++)
+    {
+        for(int col = 0 ; col<9 ;col++)
+        {
+             // if element is not there then insert that 
+        if (intSet.find(Curr->numbers[row][col]) == intSet.end()) 
+            intSet.insert(Curr->numbers[row][col]);
+        else
+            duplicate.insert(Curr->numbers[row][col]); 
+        }
+
+        fitness = fitness + intSet.size();
+        intSet.clear();
+        duplicate.clear();
+
+    }
+
+
+    //columns
+     for(int col = 0 ; col<9 ; col++)
+    {
+        for(int row = 0 ; row<9 ;row++)
+        {
+             // if element is not there then insert that 
+        if (intSet.find(Curr->numbers[row][col]) == intSet.end()) 
+            intSet.insert(Curr->numbers[row][col]);
+        else
+            duplicate.insert(Curr->numbers[row][col]); 
+        }
+
+        fitness = fitness + intSet.size();
+        intSet.clear();
+        duplicate.clear();
+
+    }
+
+
+    //squares
+
+    int start;
+    int end;
+    int startC;
+    int endC;
+    int count=0;
+
+    for(int squares = 0 ; squares<9 ; squares++)
+    {
+        if(squares < 3)
+        {
+            start = 0;
+            end = 3;
+            startC=0+count;
+            endC=3+count;
+        }
+        if(squares>=3 && squares<6)
+        {
+            start = 3;
+            end = 6;
+        }
+        if(squares>=6 && squares<9)
+        {
+            start = 6;
+            end = 9;
+        }
+
+
+        for(int row = start ; row<end ; row++)
+        {
+            for(int col = startC ; col<endC ; col++)
+            {
+                if (intSet.find(Curr->numbers[row][col]) == intSet.end()) 
+                     intSet.insert(Curr->numbers[row][col]);
+                else
+                     duplicate.insert(Curr->numbers[row][col]); 
+            }
+
+
+            }
+            fitness = fitness + intSet.size();
+            intSet.clear();
+            duplicate.clear();
+            count+=3;
+
+            if(count>6)
+            count=0;
+
+        }
+
+        return fitness;
+
+
+    }
+
+
+
+
+
+
 
 inline int geneticAlgorithm::random()
 {
